520825c6f00f0782a92c8fa3ae05fe1dc6628c3b
Nelativity <117445605+DanielaHaeussler@users.noreply.github.com>
2023-01-09 14:08:02 +0100

    public class text with strings and timestamp

---
+++ com/Text.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.sql.Timestamp;
+
+public class Text {
+ private String subjectLine;
+ private String applicationText;
+ private String greetingLine;
+ private String signature;
+ private String place;
+ private Timestamp currTimestamp; 
+}
