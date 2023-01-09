70713d116dcd391e9cb1d52f57e3f082a4f4730c
Nelativity <117445605+DanielaHaeussler@users.noreply.github.com>
2023-01-09 14:11:34 +0100

    Constructor

--- com/Text.java
+++ com/Text.java
@@ -8,5 +8,15 @@ public class Text {
  private String greetingLine;
  private String signature;
  private String place;
- private Timestamp currTimestamp; 
+ private Timestamp currTimestamp;
+
+public Text(String subjectLine, String applicationText, String greetingLine, String signature, String place,
+        Timestamp currTimestamp) {
+    this.subjectLine = subjectLine;
+    this.applicationText = applicationText;
+    this.greetingLine = greetingLine;
+    this.signature = signature;
+    this.place = place;
+    this.currTimestamp = currTimestamp;
+} 
 }
